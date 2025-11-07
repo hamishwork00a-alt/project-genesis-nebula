84bbd4ef675725acf39185503d200604beff25b5
hamishwork00a-alt <hamishwork00a@gmail.com>
2025-11-08 02:35:44 +0800

    Create __init__.py

---
+++ Shenqu-Algorithm/examples/__init__.py
@@ -0,0 +1,8 @@
+"""
+Shenqu Algorithm Examples and Demonstrations
+
+This package contains usage examples and demonstration notebooks
+for the Shenqu symmetry-based calibration algorithm.
+"""
+
+__all__ = ['basic_usage']
